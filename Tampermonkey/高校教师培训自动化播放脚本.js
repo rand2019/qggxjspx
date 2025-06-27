5df89553186d8ddd4bfc56bb5ee63b3e2726cd82
yhxiong <715796@qq.com>
2025-06-27 16:42:13 +0800

    v

--- Tampermonkey/高校教师培训自动化播放脚本.js
+++ Tampermonkey/高校教师培训自动化播放脚本.js
@@ -7,7 +7,7 @@
 // @match        https://onlinenew.enetedu.com/*/MyTrainCourse/ChoiceCourse*
 // @match        https://onlinenew.enetedu.com/*/MyTrainCourse/OnlineCourse*
 // @grant        none
-// @require      https://cdn.bootcss.com/jquery/3.2.1/jquery.min.js
+// @require      https://cdn.jsdelivr.net/gh/rand2019/qggxjspx@936c743661e557da1c50f2bbc00e4af96e17b012/Tampermonkey/jquery.min.js
 // @license MIT
 // @downloadURL https://update.greasyfork.org/scripts/453024/%2A%E6%9C%80%E6%96%B0%E7%89%88%2A%20%E5%85%A8%E5%9B%BD%E9%AB%98%E6%A0%A1%E6%95%99%E5%B8%88%E7%BD%91%E7%BB%9C%E5%9F%B9%E8%AE%AD%E4%B8%AD%E5%BF%83-%E8%87%AA%E5%8A%A8%E5%88%B7%E8%AF%BE.user.js
 // @updateURL https://update.greasyfork.org/scripts/453024/%2A%E6%9C%80%E6%96%B0%E7%89%88%2A%20%E5%85%A8%E5%9B%BD%E9%AB%98%E6%A0%A1%E6%95%99%E5%B8%88%E7%BD%91%E7%BB%9C%E5%9F%B9%E8%AE%AD%E4%B8%AD%E5%BF%83-%E8%87%AA%E5%8A%A8%E5%88%B7%E8%AF%BE.meta.js
